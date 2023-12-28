f58dd7d11ed681f58424b67a84ef15a881a62cc4
João Meirinho <meirinhojoao2004@gmail.com>
2023-12-28 20:08:30 -0300

    concluído seção Interfaces

---
+++ SECAO14/Interfaces/ExInterface/Services/ContractService.cs
@@ -0,0 +1,32 @@
+﻿using ExInterface.Entities;
+using System;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExInterface.Services
+{
+    class ContractService
+    {
+        private IPayment _payment;
+
+        public ContractService(IPayment payment)
+        {
+            _payment = payment;
+        }
+
+        public void ProcessContract(Contract contract, int months)
+        {
+            double basicQuota = contract.TotalValue / months;
+            for (int i = 1; i <= months; i++)
+            {
+                DateTime date = contract.Date.AddMonths(i);
+                double updatedQuota = basicQuota + _payment.Interest(basicQuota, i);
+                double fullQuota = updatedQuota + _payment.PaymentFee(updatedQuota);
+                contract.AddInstallment(new Installment(date, fullQuota));
+            }
+        }
+    }
+}
---
+++ SECAO14/Interfaces/IComparable/Program.cs
@@ -0,0 +1,21 @@
+﻿using ICompar.Entities;
+
+string path = @"c:\temp\in.txt";
+
+try
+{
+    using StreamReader sr = File.OpenText(path);
+    List<Employee> list = new List<Employee>();
+    while (!sr.EndOfStream)
+    {
+        list.Add(new Employee(sr.ReadLine()));
+    }
+    list.Sort();
+    foreach(Employee line in list)
+    {
+        Console.WriteLine(line);
+    }
+}catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
