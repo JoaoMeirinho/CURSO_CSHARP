4f41a143f5bab4be7dd06ad26e4e52fb21115127
João Meirinho <meirinhojoao2004@gmail.com>
2024-01-11 22:39:34 -0300

    Section 15

---
+++ SECAO15/GenericSetDictonary/Generic/Program.cs
@@ -0,0 +1,15 @@
+﻿using Generic;
+
+PrintService<int> printService = new();
+
+Console.Write("How many values? ");
+int n = int.Parse(Console.ReadLine());
+
+for (int i=0; i< n; i++)
+{
+    int x = int.Parse(Console.ReadLine());
+    printService.AddValue(x);
+}
+printService.Print();
+Console.WriteLine("First: " + printService.First());
+
---
+++ SECAO15/GenericSetDictonary/Gethashcode/Program.cs
@@ -0,0 +1,9 @@
+﻿using Gethashcode;
+
+Client a = new Client { Name = "Maria", Email = "maria@gmail.com" };
+Client b = new Client { Name = "Alex", Email = "alex@gmail.com" };
+
+Console.WriteLine(a.Equals(b));
+Console.WriteLine(a == b);
+Console.WriteLine(a.GetHashCode());
+Console.WriteLine(b.GetHashCode());
