a44e3bf82b689d8f8510e54848bdde08d4d30e80
João Meirinho <meirinhojoao2004@gmail.com>
2024-01-17 20:27:23 -0300

    solved problem ExPropostoDictionary and finished the section 15

---
+++ SECAO15/GenericSetDictonary/ExPropostoDictionary/Program.cs
@@ -0,0 +1,29 @@
+﻿string path = @"c:\temp\in.txt";
+Dictionary<string, int> people = new Dictionary<string, int>();
+
+try
+{
+    using StreamReader sr = new StreamReader(path);
+    while (!sr.EndOfStream)
+    {
+        string line = sr.ReadLine();
+        string[] name = line.Split(',');
+        if (people.ContainsKey(name[0]))
+        {
+            people[name[0]] += int.Parse(name[1]);
+        }
+        else
+        {
+            people.Add(name[0], int.Parse(name[1]));
+        }
+    }
+}catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+foreach(KeyValuePair<string, int> entry in people)
+{
+    Console.WriteLine(entry.Key + ": " + entry.Value);
+}
+
